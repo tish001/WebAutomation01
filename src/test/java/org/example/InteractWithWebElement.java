294711defdc402b9506f6d103ee0625c6069c5b4
tish001 <tahminatish001@gmail.com>
2025-08-09 23:45:32 +0600

    issue_fixed

--- src/test/java/org/example/InteractWithWebElement.java
+++ src/test/java/org/example/InteractWithWebElement.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -10,46 +11,50 @@ public class InteractWithWebElement extends DriverSetup {
 
         browser.get("https://othoba.com/");
         browser.findElement(By.linkText("Register")).click();
-        WebElement header = browser.findElement(By.tagName("h1"));
+
+        WebElement header = browser.findElement(By.xpath("//h1[normalize-space()='Register']"));
         String first_header = header.getText();
         System.out.println(first_header);
 
+
+
+        WebElement radio_label_1 = browser.findElement(By.xpath("//label[@for='gender-Male']"));
+
+
+        System.out.println("Display Status: " + radio_label_1.isDisplayed());
+        System.out.println("Select Status: " + radio_label_1.isSelected());
+        Thread.sleep(2000);
+        radio_label_1.click();
+        System.out.println("After click Select Status: " + radio_label_1.isSelected());
+        Thread.sleep(2000);
+
+        WebElement radio_label_2 = browser.findElement(By.xpath("//label[@for='gender-Female']"));
+        Thread.sleep(2000);
+
+        radio_label_2.click();
+
+        System.out.println("After click another one Select Status for radio button 1: " + radio_label_2.isSelected());
         WebElement autocomplete_input_box = browser.findElement(By.xpath("//input[@id='autocomplete-0-input']"));
 
         autocomplete_input_box.sendKeys("book");
 
-        Thread.sleep(5000);
+        Thread.sleep(2000);
+        autocomplete_input_box.sendKeys(Keys.CONTROL + "a");
 
-        autocomplete_input_box.clear();
-        Thread.sleep(5000);
-
-        autocomplete_input_box.sendKeys("grocery");
+        autocomplete_input_box.sendKeys(Keys.DELETE);
+        Thread.sleep(2000);
+        autocomplete_input_box.sendKeys("Grocery");
         Thread.sleep(2000);
         String text = autocomplete_input_box.getAttribute("placeholder");
 
         System.out.println("placeholder: " + text);
         Thread.sleep(3000);
-       WebElement submitButton = browser.findElement(By.id("register-button"));
-     String background_color = submitButton.getCssValue("background-color");
-      System.out.println(background_color);
-        //     browser.get("https://rahulshettyacademy.com/AutomationPractice/");
-//
-//        WebElement radio_option_1 = browser.findElement(By.xpath("//input[@name='gender']"));
-//    System.out.println("Display Status: " + radio_option_1.isDisplayed());
-//    System.out.println("Select Status: " + radio_option_1.isSelected());
-//       Thread.sleep(2000);
-//     radio_option_1.click();
-//   System.out.println("After click Select Status: " + radio_option_1.isSelected());
-//    Thread.sleep(2000);
-//
-//        WebElement radio_option_2 = browser.findElement(By.xpath("//input[@value='radio2']"));
-//
-//
-//        radio_option_2.click();
-//
-//        System.out.println("After click another one Select Status for radio button 1: " + radio_option_1.isSelected());
+        WebElement submitButton = browser.findElement(By.id("register-button"));
+        String background_color = submitButton.getCssValue("background-color");
+        System.out.println(background_color);
+        Thread.sleep(3000);
 //        WebElement box = browser.findElement(By.id("displayed-text"));
-//
+
 //        System.out.println("Before: " + box.isDisplayed());
 //
 //        browser.findElement(By.id("hide-textbox")).click();

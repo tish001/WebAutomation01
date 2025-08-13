aab33957066489252af0ea3228168edf70902bf0
tish001 <tahminatish001@gmail.com>
2025-08-13 23:18:27 +0600

    InteractWithDrowDown

---
+++ src/test/java/org/example/InteractWithDropDown.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class InteractWithDropDown extends DriverSetup {
+
+    @Test
+    public void testDropdown() throws InterruptedException {
+
+        browser.get("https://othoba.com/");
+        browser.findElement(By.linkText("Register")).click();
+//     WebElement dropdown = browser.findElement(By.xpath("//div[@class='date-picker-wrapper']"));
+        WebElement dropdown = browser.findElement((By.xpath("//select[@name='DateOfBirthDay']")));
+        dropdown.click();
+        Thread.sleep(4000);
+       Select select = new Select(dropdown);
+       List<WebElement> options = select.getOptions();
+
+        for (WebElement option: options){
+            System.out.println(option.getText());
+        }
+
+        select.selectByVisibleText("5");
+        Thread.sleep(2000);
+
+        System.out.println( select.getFirstSelectedOption().getText());
+
+        select.selectByValue("20");
+        Thread.sleep(2000);
+        System.out.println( select.getFirstSelectedOption().getText());
+
+        select.selectByIndex(1);
+
+        Thread.sleep(2000);
+        System.out.println( select.getFirstSelectedOption().getText());
+        WebElement dropdownHover = browser.findElement(By.className("w-icon-category"));
+
+        Actions actions = new Actions(browser);
+        actions.moveToElement(dropdownHover).perform(); // hover
+        Thread.sleep(4000);
+
+        List<WebElement> dropdownHoverLink = browser.findElements(
+                By.xpath("//ul[contains(@class, 'vertical-menu')]/li/a")
+        );
+        List<WebElement> dropdownHoverLinks = browser.findElements(
+                By.className("dropdown-box")
+        );
+
+        for (WebElement link : dropdownHoverLinks) {
+            System.out.println(link.getText());
+        }
+    }
+
+}

96333d3a2406ade5688a6293c2b1ee628ccb4a26
tish001 <tahminatish001@gmail.com>
2025-08-14 23:03:19 +0600

    dropdown_checkbox_update

--- src/test/java/org/example/InteractWithDropDown.java
+++ src/test/java/org/example/InteractWithDropDown.java
@@ -12,11 +12,12 @@ public class InteractWithDropDown extends DriverSetup {
 
     @Test
     public void testDropdown() throws InterruptedException {
-
+        WebElement modalImage;
         browser.get("https://othoba.com/");
+        modalImage = browser.findElement(By.className("mfp-close"));
+       modalImage.click();
         browser.findElement(By.linkText("Register")).click();
-//     WebElement dropdown = browser.findElement(By.xpath("//div[@class='date-picker-wrapper']"));
-        WebElement dropdown = browser.findElement((By.xpath("//select[@name='DateOfBirthDay']")));
+        WebElement dropdown = browser.findElement(By.xpath("//select[@name='DateOfBirthDay']"));
         dropdown.click();
         Thread.sleep(4000);
        Select select = new Select(dropdown);
@@ -35,10 +36,39 @@ public class InteractWithDropDown extends DriverSetup {
         Thread.sleep(2000);
         System.out.println( select.getFirstSelectedOption().getText());
 
-        select.selectByIndex(1);
+//        select.selectByIndex(1);
+//
+//        Thread.sleep(2000);
+//        System.out.println( select.getFirstSelectedOption().getText());
+        WebElement dropdownMonth = browser.findElement(By.xpath("//select[@name='DateOfBirthMonth']"));
 
+        dropdownMonth.click();
+        Thread.sleep(4000);
+        Select selectMonth = new Select(dropdownMonth);
+        List<WebElement> optionsMonth = selectMonth.getOptions();
+
+        for (WebElement option: optionsMonth){
+            System.out.println(option.getText());
+        }
+
+        selectMonth.selectByVisibleText("March");
         Thread.sleep(2000);
-        System.out.println( select.getFirstSelectedOption().getText());
+
+        System.out.println( selectMonth.getFirstSelectedOption().getText());
+        WebElement dropdownYear= browser.findElement(By.xpath("//select[@name='DateOfBirthYear']"));
+        dropdownYear.click();
+        Thread.sleep(3000);
+        Select selectYear= new Select(dropdownYear);
+        List<WebElement> optionsYear = selectYear.getOptions();
+
+        for (WebElement option:optionsYear)
+        {
+            System.out.println(option.getText());
+        }
+       selectYear.selectByVisibleText("1996");
+        Thread.sleep(3000);
+        System.out.println(selectYear.getFirstSelectedOption().getText());
+
         WebElement dropdownHover = browser.findElement(By.className("w-icon-category"));
 
         Actions actions = new Actions(browser);
--- src/test/java/org/example/InteractWithWebElement.java
+++ src/test/java/org/example/InteractWithWebElement.java
@@ -8,18 +8,19 @@ import org.testng.annotations.Test;
 public class InteractWithWebElement extends DriverSetup {
     @Test
     public void interactWithElement() throws InterruptedException {
+        WebElement modalImage;
 
         browser.get("https://othoba.com/");
+        modalImage = browser.findElement(By.className("mfp-close"));
+        modalImage.click();
         browser.findElement(By.linkText("Register")).click();
-
+//        WebElement h1 = browser.findElement(By.tagName("h1"));
+//        String first_header = h1.getText();
+//        System.out.println(first_header);
         WebElement header = browser.findElement(By.xpath("//h1[normalize-space()='Register']"));
         String first_header = header.getText();
         System.out.println(first_header);
-
-
-
-        WebElement radio_label_1 = browser.findElement(By.xpath("//label[@for='gender-Male']"));
-
+      WebElement radio_label_1 = browser.findElement(By.xpath("//label[@for='gender-Male']"));
 
         System.out.println("Display Status: " + radio_label_1.isDisplayed());
         System.out.println("Select Status: " + radio_label_1.isSelected());
@@ -34,7 +35,16 @@ public class InteractWithWebElement extends DriverSetup {
         radio_label_2.click();
 
         System.out.println("After click another one Select Status for radio button 1: " + radio_label_2.isSelected());
+        Thread.sleep(2000);
+        WebElement checkBox2 = browser.findElement(By.id("Newsletter"));
+
+        System.out.println("Is enabled :" +checkBox2.isEnabled());
+
+        checkBox2.click();
+
+        Thread.sleep(3000);
         WebElement autocomplete_input_box = browser.findElement(By.xpath("//input[@id='autocomplete-0-input']"));
+
 
         autocomplete_input_box.sendKeys("book");
 
@@ -46,7 +56,6 @@ public class InteractWithWebElement extends DriverSetup {
         autocomplete_input_box.sendKeys("Grocery");
         Thread.sleep(2000);
         String text = autocomplete_input_box.getAttribute("placeholder");
-
         System.out.println("placeholder: " + text);
         Thread.sleep(3000);
         WebElement submitButton = browser.findElement(By.id("register-button"));
@@ -54,21 +63,16 @@ public class InteractWithWebElement extends DriverSetup {
         System.out.println(background_color);
         Thread.sleep(3000);
 //        WebElement box = browser.findElement(By.id("displayed-text"));
-
+//
 //        System.out.println("Before: " + box.isDisplayed());
 //
 //        browser.findElement(By.id("hide-textbox")).click();
 //
 //        Thread.sleep(3000);
-//
+
 //        System.out.println("After: " + box.isDisplayed());
-//        Thread.sleep(3000);
-//        WebElement checkBox2 = browser.findElement(By.id("checkBoxOption2"));
-//
-//        System.out.println("Is enabled :" +checkBox2.isEnabled());
-//
-//        checkBox2.click();
 //        Thread.sleep(2000);
+
 
     }
 }
